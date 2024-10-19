b3897baf670e61d10fa5418053a6f55397b2b47e
John-09 <johnsd2709@gmail.com>
2024-10-20 01:21:19 +0530

    Added projects

--- portfolio-website/src/Pages/Dashboard.tsx
+++ portfolio-website/src/Pages/Dashboard.tsx
@@ -3,30 +3,30 @@
 import React from 'react'
 import { Coffee, Database, Layout, Menu, Server, Smartphone } from 'lucide-react'
 import { Button } from "@/components/ui/button"
-import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards"
-import supplyChain from "@/assets/suuplyChain.jpeg"
-import { Code, Code2, } from 'lucide-react'
+// import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards"
+// import supplyChain from "@/assets/suuplyChain.jpeg"
+import { Code, Code2,ArrowRight } from 'lucide-react'
 
-const testimonials = [
-  {
-    image: supplyChain,
-    name: "Chaincare",
-    title: "Secure Health Data Management System",
-  },
-  {
-    // quote:
-    //   "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    image: supplyChain,
-    name: "HyperBase",
-    title: "Secure Platform As A Service Using Blockchain",
-  },
-  {
-    image: supplyChain,
-    name: "SupplyChain Management",
-    title: "A Permissioned Blockchain System for SupplyChain Management",
-  },
+// const testimonials = [
+//   {
+//     image: supplyChain,
+//     name: "Chaincare",
+//     title: "Secure Health Data Management System",
+//   },
+//   {
+//     // quote:
+//     //   "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+//     image: supplyChain,
+//     name: "HyperBase",
+//     title: "Secure Platform As A Service Using Blockchain",
+//   },
+//   {
+//     image: supplyChain,
+//     name: "SupplyChain Management",
+//     title: "A Permissioned Blockchain System for SupplyChain Management",
+//   },
 
-];
+// ];
 
 interface Feature {
   icon: React.ReactNode;
@@ -35,7 +35,7 @@ interface Feature {
 }
 
 
-const features: Feature[] = [
+const programming: Feature[] = [
   {
     icon: <Code className="h-6 w-6" />,  // Using Code icon for JavaScript
     title: "JavaScript",
@@ -51,6 +51,11 @@ const features: Feature[] = [
     title: "Java",
     description: "A powerful, platform-independent programming language used for building large-scale applications."
   },
+  
+];
+
+
+const frameworks: Feature[] = [
   {
     icon: <Smartphone className="h-6 w-6" />,  // Using Smartphone icon for React.js
     title: "React.js",
@@ -65,7 +70,10 @@ const features: Feature[] = [
     icon: <Server className="h-6 w-6" />,  // Using Server icon for Node.js
     title: "Node.js",
     description: "A runtime environment that allows JavaScript to be used for server-side development, enabling full-stack development."
-  },
+  }
+]
+
+const databases: Feature[] = [
   {
     icon: <Database className="h-6 w-6" />,  // Using Database icon for MySQL
     title: "MySQL",
@@ -76,7 +84,7 @@ const features: Feature[] = [
     title: "PostgreSQL",
     description: "An advanced, open-source relational database known for its extensibility and support for complex queries."
   }
-];
+]
 
 
 
@@ -113,23 +121,72 @@ export default function Dashboard() {
         </h1>
       </main>
 
-      <div className="py-16">
+      {/* <div className="py-16">
         <InfiniteMovingCards
          items={testimonials}
          direction="right"
          speed="slow"
         />
-      </div>
+      </div> */}
 
       <div>
-      <Button variant="outline" className="rounded-full px-6">Stacks</Button>
+      <Button variant="outline"  className="rounded-full px-10 py-6 bg-black text-gray-100 text-lg">Skills</Button>
+      </div>
+
+      <div className='pt-6'>
+      <Button variant="outline" className="rounded-full px-6">Programming Languages</Button>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-5">
-          {features.map((feature, index) => (
-            <FeatureCard key={index} feature={feature} />
+          {programming.map((programming, index) => (
+            <FeatureCard key={index} feature={programming} />
           ))}
         </div>
+
+      <div className='pt-6'>
+      <Button variant="outline" className="rounded-full px-6 pt-2">Frameworks/Libraries</Button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-5">
+          {frameworks.map((frameworks, index) => (
+            <FeatureCard key={index} feature={frameworks} />
+          ))}
+        </div>
+
+
+        <div className='pt-6'>
+      <Button variant="outline" className="rounded-full px-6 pt-2">Database</Button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-5">
+          {databases.map((databases, index) => (
+            <FeatureCard key={index} feature={databases} />
+          ))}
+        </div>
+
+
+        <div className='pt-6'>
+          <Button 
+            variant="outline" 
+            className="rounded-full px-10 py-6 bg-black text-gray-100 text-lg"
+          >
+            Projects
+          </Button>
+        </div>
+
+        <div>
+          <div className='flex flex-row'>
+            <div className="text-2xl font-bold flex justify-start pt-6">CHAINCARE</div>
+            <div className="pt-6 flex ml-auto">
+              <button className="flex items-center justify-center w-16 h-16 rounded-full border-2 border-black">
+                <ArrowRight className="w-6 h-6 text-black" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+
     </div>
   )
 }
